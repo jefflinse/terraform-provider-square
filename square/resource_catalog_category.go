f5b386ad438f5b42f33b6420f2898fe511aaee59
Jeff Linse <jeff@jefflinse.com>
2021-02-21 10:57:38 -0800

    Reorganize catalog category logic

--- square/resource_catalog_category.go
+++ square/resource_catalog_category.go
@@ -1,7 +1,10 @@
 package square
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform/helper/schema"
+	squaremodel "github.com/jefflinse/square-connect/models"
 )
 
 func resourceSquareCatalogCategory() *schema.Resource {
@@ -20,36 +23,44 @@ func resourceSquareCatalogCategory() *schema.Resource {
 }
 
 func resourceSquareCatalogCategoryCreate(d *schema.ResourceData, meta interface{}) error {
-	square := meta.(*Client)
-	name := d.Get("name").(string)
-
-	id, err := square.CreateCatalogCategory(name)
+	categoryID := newTempID()
+	created, err := meta.(*Client).upsertCatalogObject(&squaremodel.CatalogObject{
+		ID:           &categoryID,
+		Type:         strPtr("CATEGORY"),
+		CategoryData: createCatalogCategory(d),
+	})
 	if err != nil {
-		return err
+		return fmt.Errorf("create catalog category: %w", err)
 	}
 
-	d.SetId(id)
+	d.SetId(*created.ID)
+
 	return resourceSquareCatalogCategoryRead(d, meta)
 }
 
 func resourceSquareCatalogCategoryRead(d *schema.ResourceData, meta interface{}) error {
-	square := meta.(*Client)
-	c, err := square.RetrieveCatalogCategory(d.Id())
+	obj, err := meta.(*Client).retrieveCatalogObject(d.Id())
 	if err != nil {
 		return err
 	}
 
-	d.Set("name", c.Name)
-
-	return nil
+	return readCatalogCategory(obj.CategoryData, d)
 }
 
 func resourceSquareCatalogCategoryUpdate(d *schema.ResourceData, meta interface{}) error {
 	if d.HasChange("name") {
-		square := meta.(*Client)
-		name := d.Get("name").(string)
-		_, err := square.UpdateCatalogCategory(d.Id(), name)
+		client := meta.(*Client)
+		obj, err := client.retrieveCatalogObject(d.Id())
 		if err != nil {
+			return err
+		}
+
+		if _, err := client.upsertCatalogObject(&squaremodel.CatalogObject{
+			ID:           strPtr(*obj.ID),
+			Type:         strPtr("CATEGOORY"),
+			Version:      obj.Version,
+			CategoryData: createCatalogCategory(d),
+		}); err != nil {
 			return err
 		}
 	}
@@ -58,7 +69,17 @@ func resourceSquareCatalogCategoryUpdate(d *schema.ResourceData, meta interface{
 }
 
 func resourceSquareCatalogCategoryDelete(d *schema.ResourceData, meta interface{}) error {
-	square := meta.(*Client)
-	_, err := square.DeleteCatalogObject(d.Id())
+	_, err := meta.(*Client).DeleteCatalogObject(d.Id())
 	return err
+}
+
+func createCatalogCategory(d *schema.ResourceData) *squaremodel.CatalogCategory {
+	return &squaremodel.CatalogCategory{
+		Name: d.Get("name").(string),
+	}
+}
+
+func readCatalogCategory(category *squaremodel.CatalogCategory, d *schema.ResourceData) error {
+	d.Set("name", category.Name)
+	return nil
 }
